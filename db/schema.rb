43498e520d1531e36f97bcef2037db041c290494
abood jallad <aboodjallad@aboods-MacBook-Pro.local>
2025-09-03 22:09:42 +0300

    add a forgin key 'author_id' to the book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_03_124827) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_03_174712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -27,10 +27,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_03_124827) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
-    t.string "auther"
     t.text "description"
     t.integer "number_of_pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
+
+  add_foreign_key "books", "authors"
 end
